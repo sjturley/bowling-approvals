c9f2daf6d6cae8e7444aaada8c52b42ea1c5596e
Steve Turley <steve@turleylabs.com>
2021-02-04 09:29:09 -0600

    Handling spare in 10th frame

--- src/main/java/bowling/approvals/TenthFrame.java
+++ src/main/java/bowling/approvals/TenthFrame.java
@@ -14,18 +14,34 @@ public class TenthFrame implements Frame {
         if (rolls != null) {
             String firstRoll = rolls.get(0).toString();
             String secondRoll = rolls.size() > 1 ? rolls.get(1).toString() : "_";
-            return "| " + firstRoll + "|" + secondRoll + "|_|\n";
+            String thirdRoll = rolls.size() > 2 ? rolls.get(2).toString() : "_";
+            return "| " + firstRoll + "|" + secondRoll + "|" + thirdRoll + "|\n";
         }
         return "|  |_|_|\n";
+    }
+
+    private boolean hasAnotherRoll() {
+        return this.rolls.size() > 2;
     }
 
     @Override
     public String getScoreBox() {
         if (lastScore != null && rolls != null && rolls.size() >= 2) {
-            String score = this.leftPad(lastScore + rolls.get(0) + rolls.get(1));
+            String score = "";
+            if (sumOfRolls() == 10 && !hasAnotherRoll()) {
+                score = "  ";
+            } else if(sumOfRolls() == 10) {
+                score = this.leftPad(lastScore + sumOfRolls() + this.rolls.get(2));
+            } else {
+                score = this.leftPad(lastScore + sumOfRolls());
+            }
             return "| " + score + "   |\n";
         }
         return "|      |\n";
+    }
+
+    private int sumOfRolls() {
+        return rolls.get(0) + rolls.get(1);
     }
 
     public void setRolls(List<Integer> rolls) {
